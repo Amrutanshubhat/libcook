3076a7c85a8920b7b8fc29cc050074dd7b093174
Amrutanshu <amrubht11@gmail.com>
2024-12-18 12:55:14 +0530

    renames

--- src/impl_libcook.c
+++ src/impl_libcook.c
@@ -20,25 +20,28 @@
 typedef struct {
   float position[3];
   float color[4];
+  float texture[2];
 } Vertex;
 
 static struct {
   unsigned int width;
   unsigned int height;
-  char title[32];
   GLFWwindow *handle;
 } _window;
 
+unsigned int *const window_width = &_window.width;
+unsigned int *const window_height = &_window.height;
+
 enum Gl_modes{
-	POLYGON_MODE, LINE_MODE, POINT_MODE
+	POLYGON_MODE, LINE_MODE, POINT_MODE, MODE_SENTINEL
 };
 
 /*
  * eventhough memory is allocated in heap, the buffer size is limited to the defines below.
  * Exceeding the allocation during the runtime causes the program to abort.
  * */
-#define MAX_VERTICES 3000
-#define MAX_INDICES 4500
+#define MAX_VERTICES 30000
+#define MAX_INDICES 45000
 static struct _gl_mode{
 	size_t vert_capacity;
 	size_t idx_capacity;
@@ -46,7 +49,7 @@ static struct _gl_mode{
 	size_t idx_cnt;
 	Vertex* vertices;
 	unsigned int* indices;
-} _gl_mode[POINT_MODE+1];
+} _gl_mode[MODE_SENTINEL];
 
 static struct {
   unsigned int vbo;
@@ -109,11 +112,27 @@ static unsigned int compile_shader(const char *filename, unsigned int type) {
     glGetShaderiv(id, GL_INFO_LOG_LENGTH, &len);
     char err[len];
     glGetShaderInfoLog(id, len, &len, err);
-    assert(true, "Shader Compilation error: %s\n", err);
+    assert(status, "Shader Compilation error: %s\n", err);
   }
 
   free(code);
   return id;
+}
+
+void update_fps() {
+	static double ps;
+	static int frame_cnt;
+	double cur_sec = glfwGetTime();
+	double elapsed = cur_sec-ps;
+	if (elapsed > 1) {
+		ps = cur_sec;
+		int fps = frame_cnt/elapsed;
+		char title[10] = {0};
+		sprintf(title, "%d", fps);
+		glfwSetWindowTitle(_window.handle, title);
+		frame_cnt = 0;
+	}
+	frame_cnt++;
 }
 
 // Callbacks
@@ -151,7 +170,7 @@ void CreateWindow(const int width, const int height, const char *title) {
   glfwMakeContextCurrent(_window.handle);
   glfwSetKeyCallback(_window.handle, key_callback);
   glfwSetFramebufferSizeCallback(_window.handle, framebuffer_size_callback);
-  glfwSwapInterval(1); // limit fps to 60
+  glfwSwapInterval(2); // 1 to limit fps to 60
 
   gladLoadGL(glfwGetProcAddress);
   glGenVertexArrays(1, &_gl.vao);
@@ -168,11 +187,17 @@ void CreateWindow(const int width, const int height, const char *title) {
                GL_DYNAMIC_DRAW);
 
   // Note: Beware of these hardcoded numbers
+  //position
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
   glEnableVertexAttribArray(0);
 
+  //color
   glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)(3*sizeof(float)));
   glEnableVertexAttribArray(1);
+
+  //texture
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)(7*sizeof(float)));
+  //glEnableVertexAttribArray(2);
 
   const char *vertex_prog_file = "src/vertex.glsl";
   const char *fragment_prog_file = "src/fragment.glsl";
@@ -181,7 +206,6 @@ void CreateWindow(const int width, const int height, const char *title) {
 
   _window.width = width;
   _window.height = height;
-  strncpy(_window.title, title, sizeof(_window.title) - 1);
 }
 
 unsigned int CreateShaderProgram(const char *const vertex_glsl,
@@ -198,28 +222,27 @@ unsigned int CreateShaderProgram(const char *const vertex_glsl,
   glValidateProgram(sp);
   int status;
   glGetProgramiv(sp, GL_VALIDATE_STATUS, &status);
-  assert(status, "Shader program linking failed\n")
+  if (status == GL_FALSE) {
+	  int max_len = 256;
+	  char log[max_len]; memset(log, 0, sizeof(log));
+	  glGetProgramInfoLog(sp, max_len, nullptr, log);
+	  assert(status, "Shader program linking failed: %u\n%s\n", sp, log);
+  }
       // cleanups
       glDeleteShader(vs);
   glDeleteShader(fs);
-
   return sp;
 }
 
-void DrawRectangle(const Vec2 pos, const int width, const int height,
-                   const Color col) {
+void DrawRectangle(const Vec2 pos, const Vec2 dim, const Color col) {
   Vec3 v[4] = {{.x = (2.0 * pos.x / _window.width) - 1,
-                .y = (-2.0 * pos.y / _window.height) + 1,
-                .z = 0.0f},
+                .y = (-2.0 * pos.y / _window.height) + 1},
                {.x = (2.0 * pos.x / _window.width) - 1,
-                .y = (-2.0 * (pos.y + height) / _window.height) + 1,
-                .z = 0.0f},
-               {.x = (2.0 * (pos.x + width) / _window.width) - 1,
-                .y = (-2.0 * (pos.y + height) / _window.height) + 1,
-                .z = 0.0f},
-               {.x = (2.0 * (pos.x + width) / _window.width) - 1,
-                .y = (-2.0 * pos.y / _window.height) + 1,
-                .z = 0.0f}};
+                .y = (-2.0 * (pos.y + dim.y) / _window.height) + 1},
+               {.x = (2.0 * (pos.x + dim.x) / _window.width) - 1,
+                .y = (-2.0 * (pos.y + dim.y) / _window.height) + 1},
+               {.x = (2.0 * (pos.x + dim.x) / _window.width) - 1,
+                .y = (-2.0 * pos.y / _window.height) + 1}};
 
   unsigned int idx[] = { 0, 1, 2, 2, 3, 0 };
   enum Gl_modes mode = POLYGON_MODE;
@@ -229,68 +252,72 @@ void DrawRectangle(const Vec2 pos, const int width, const int height,
     vertex_append(v[i], col, mode);
 }
 
-void DrawLine(const Vec2 start, const Vec2 end, const Color color, const int thickness) {
-	//TODO
+void DrawLine(const Vec2 start, const Vec2 end, const Color col) {
+	Vec3 v[2] = {
+		{.x=(2.0*start.x/_window.width)-1, 
+		 .y=(-2.0*start.y/_window.height)+1}, 
+		{.x=(2.0*end.x/_window.width)-1, 
+		 .y=(-2.0*end.y/_window.height)+1}
+	};
+	unsigned int idx[] = {0, 1};
+	enum Gl_modes mode = LINE_MODE;
+	index_append(idx, sizeof(idx)/sizeof(*idx), mode);
+	for (size_t i=0; i<2; i++)
+		vertex_append(v[i], col, mode);
 }
 
 inline void EndCooking() {
   // clear screen
-  glClearColor(0.06f, 0.06f, 0.06f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
-  glUseProgram(_gl.shader_program);
-  glBindVertexArray(_gl.vao);
-  glBindBuffer(GL_ARRAY_BUFFER, _gl.vbo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _gl.ebo);
-  
-  // copy all gl modes data
-  struct _gl_mode* ptr = nullptr;
-  size_t tot_vcap=0, tot_icap=0;
+  //glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT); 
+	 glUseProgram(_gl.shader_program);
+	 glBindVertexArray(_gl.vao);
+	 glBindBuffer(GL_ARRAY_BUFFER, _gl.vbo);
+	 glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _gl.ebo);
 
-  for (size_t i=0, offset=0; i<=POINT_MODE; i++) {
+	 // copy all gl modes data
+	 struct _gl_mode* ptr = nullptr;
+	 size_t tot_vcap=0, tot_icap=0;
+
+	 for (size_t i=0; i<MODE_SENTINEL; i++) {
+	  ptr = &_gl_mode[i];
+	  if (!ptr->vert_cnt) continue;
+
 	  int draw_mode;
 	  switch(i) {
 		  case 0: draw_mode=GL_TRIANGLES; break;
-		  case 1: draw_mode=GL_LINE; break; 
+		  case 1: draw_mode=GL_LINES; break; 
 		  case 2: draw_mode=GL_POINTS; break; 
 		  default: assert(false, "Unknown draw call\n");
 	  }
 
-	  ptr = &_gl_mode[i];
-	  if (!ptr->vert_cnt) continue;
-
-	  glBufferSubData(GL_ARRAY_BUFFER, 
-					  tot_vcap*sizeof(*ptr->vertices), 
+	  glBufferSubData(GL_ARRAY_BUFFER, 0, 
 					  ptr->vert_cnt*sizeof(*ptr->vertices), 
 					  ptr->vertices);
 
-	  offset = tot_icap*sizeof(*ptr->indices);
-
-	  glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 
-					  offset, 
+	  glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, 
 					  ptr->idx_cnt*sizeof(*ptr->indices), 
 					  ptr->indices);
 
-	  glDrawElements(draw_mode, 
-					  ptr->idx_cnt, 
-					  GL_UNSIGNED_INT, 
-					  (const void *)offset);
+	  glDrawElements(draw_mode, ptr->idx_cnt, GL_UNSIGNED_INT, nullptr);
 
 	  tot_vcap += ptr->vert_cnt;
 	  tot_icap += ptr->idx_cnt;
 	  ptr->idx_cnt = 0;
 	  ptr->vert_cnt = 0;
-  }
-	assert(tot_vcap <= MAX_VERTICES, "Vertex Limit Exceede\n");
-	assert(tot_icap <= MAX_INDICES, "Index Limit Exceede\n");
+	 }
+	assert(tot_vcap <= MAX_VERTICES, "Vertex Limit Exceeded\n");
+	assert(tot_icap <= MAX_INDICES, "Index Limit Exceeded\n");
   glfwSwapBuffers(_window.handle);
   glfwPollEvents();
+  update_fps();
 }
 
 inline bool StartCooking() { return !glfwWindowShouldClose(_window.handle); }
 
 void CloseWindow() {
   glfwDestroyWindow(_window.handle);
-  for (size_t i=0; i<=POINT_MODE; i++) {
+  for (size_t i=0; i<MODE_SENTINEL; i++) {
 	  if (_gl_mode[i].vert_capacity) free(_gl_mode[i].vertices);
 	  if (_gl_mode[i].idx_capacity) free(_gl_mode[i].indices);
   }
