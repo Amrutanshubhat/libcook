cb65387643f65155d75f806ac6b596e2284e0c30
Amrutanshu <amrubht11@gmail.com>
2024-12-28 18:02:43 +0530

    Dynamic buffer allocation
    removed constant buffer memory

--- src/impl_libcook.c
+++ src/impl_libcook.c
@@ -1,6 +1,7 @@
+
 /*
- * vertex and index arrays are static with constant storage allocated which are
- * copied to gpu's buffer only once per frame to render all vertices.
+ * vertex and elemnt objects data are uploaded to gpu's buffer and single draw-
+ 		call is made at the end for each draw mode(3 currently).
  * */
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,69 +31,75 @@ unsigned int *const window_height = &_window.height;
 
 enum Gl_modes { TRIANGLE_MODE, LINE_MODE, POINT_MODE, MODE_SENTINEL };
 
-/*
- * eventhough memory is allocated in heap, the buffer size is limited to the
- * defines below. Exceeding the allocation during the runtime causes the program
- * to abort.
- * */
-#define MAX_VERTICES 30000
-#define MAX_INDICES 45000
 static struct _gl_mode {
+  unsigned int vbo;
+  unsigned int vao;
+  unsigned int ebo;
+  unsigned int sp;
   size_t vert_capacity;
   size_t idx_capacity;
   size_t vert_cnt;
   size_t idx_cnt;
-  Vertex *vertices;
-  unsigned int *indices;
 } _gl_mode[MODE_SENTINEL];
-
-static struct {
-  unsigned int vbo;
-  unsigned int vao;
-  unsigned int ebo;
-  unsigned int shader_program;
-} _gl;
 
 // local helper functions
 static void append_vert_ind(const Vec3 *positions, const Color *colors,
                             const int size, const unsigned int *indices,
                             const int isize, enum Gl_modes mode) {
 
-  // deal with vertex data
-  struct _gl_mode *handle = &_gl_mode[mode];
-  size_t new_vert_cnt = handle->vert_cnt + size;
-  if (handle->vert_capacity < new_vert_cnt) {
-    handle->vert_capacity += handle->vert_capacity > new_vert_cnt
-                                 ? handle->vert_capacity
-                                 : new_vert_cnt;
-    handle->vertices = realloc(handle->vertices, handle->vert_capacity *
-                                                     sizeof(*handle->vertices));
-    assert(handle->vertices, "Vertex realloc failed\nDownload more RAM\n");
+  struct _gl_mode *ptr = &_gl_mode[mode];
+
+  glBindBuffer(GL_ARRAY_BUFFER, ptr->vbo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ptr->ebo);
+  //check for vertex capacity in buffer object
+
+  if (ptr->vert_capacity < ptr->vert_cnt+size) {
+	  ptr->vert_capacity = ptr->vert_cnt+size;
+	  Vertex temp[ptr->vert_cnt];
+	  glGetBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(temp), temp);
+
+	  glBufferData(GL_ARRAY_BUFFER, 
+			  sizeof(Vertex)*ptr->vert_capacity, nullptr,
+			  GL_DYNAMIC_DRAW);
+	  glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(temp), temp);
   }
+
+  // check for elemnt buffer object
+  if (ptr->idx_capacity < ptr->idx_cnt+isize) {
+	  ptr->idx_capacity = ptr->idx_cnt+isize;
+	  unsigned int temp[ptr->idx_cnt];
+	  glGetBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(temp), temp);
+
+	  glBufferData(GL_ELEMENT_ARRAY_BUFFER, 
+			  sizeof(unsigned int)*ptr->idx_capacity,
+			  nullptr, GL_DYNAMIC_DRAW);
+	  glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, sizeof(temp), temp);
+  }
+
+  Vertex v[size];
   for (size_t i = 0; i < size; i++) {
-    handle->vertices[handle->vert_cnt].position[0] = positions[i].x;
-    handle->vertices[handle->vert_cnt].position[1] = positions[i].y;
-    handle->vertices[handle->vert_cnt].position[2] = positions[i].z;
-    handle->vertices[handle->vert_cnt].color[0] = colors[i].r;
-    handle->vertices[handle->vert_cnt].color[1] = colors[i].g;
-    handle->vertices[handle->vert_cnt].color[2] = colors[i].b;
-    handle->vertices[handle->vert_cnt].color[3] = colors[i].a;
-    handle->vert_cnt++;
+    v[i].position[0] = positions[i].x;
+    v[i].position[1] = positions[i].y;
+    v[i].position[2] = positions[i].z;
+    v[i].color[0] = colors[i].r;
+    v[i].color[1] = colors[i].g;
+    v[i].color[2] = colors[i].b;
+    v[i].color[3] = colors[i].a;
   }
-
+  glBufferSubData(GL_ARRAY_BUFFER, ptr->vert_cnt*sizeof(Vertex), sizeof(v), v);
   // deal with index data
-  size_t new_idx_cnt = handle->idx_cnt + isize;
-  if (handle->idx_capacity < new_idx_cnt) {
-    handle->idx_capacity +=
-        handle->idx_capacity > new_idx_cnt ? handle->idx_capacity : new_idx_cnt;
-    handle->indices = realloc(handle->indices,
-                              handle->idx_capacity * sizeof(*handle->indices));
-    assert(handle->indices, "Index realloc failed\n");
+  unsigned int ind[isize];
+  for (size_t i = 0; i < isize; i++) {
+    ind[i] = ptr->vert_cnt + indices[i];
   }
 
-  for (size_t i = 0, vstart = handle->vert_cnt - size; i < isize; i++) {
-    handle->indices[handle->idx_cnt++] = vstart + indices[i];
-  }
+  glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 
+				  ptr->idx_cnt*sizeof(unsigned int), sizeof(ind), ind);
+  ptr->vert_cnt += size;
+  ptr->idx_cnt += isize;
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 static char *extract_shader_source_code(const char *filename) {
@@ -178,44 +185,14 @@ void CreateWindow(const int width, const int height, const char *title) {
   glfwMakeContextCurrent(_window.handle);
   glfwSetKeyCallback(_window.handle, key_callback);
   glfwSetFramebufferSizeCallback(_window.handle, framebuffer_size_callback);
-  glfwSwapInterval(2); // 1 to limit fps to 60
+  glfwSwapInterval(0); // 1 to limit fps to 60
 
   gladLoadGL(glfwGetProcAddress);
-  glGenVertexArrays(1, &_gl.vao);
-  glGenBuffers(1, &_gl.vbo);
-  glGenBuffers(1, &_gl.ebo);
 
-  glBindVertexArray(_gl.vao);
-  glBindBuffer(GL_ARRAY_BUFFER, _gl.vbo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _gl.ebo);
-
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * MAX_INDICES,
-               nullptr, GL_DYNAMIC_DRAW);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * MAX_VERTICES, nullptr,
-               GL_DYNAMIC_DRAW);
-
-  // Note: Beware of these hardcoded numbers
-  // position
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
-  glEnableVertexAttribArray(0);
-
-  // color
-  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                        (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  // texture
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                        (void *)(7 * sizeof(float)));
-  // glEnableVertexAttribArray(2);
-
+  // Currently common shader program for all gl modes
   const char *vertex_prog_file = "src/vertex.glsl";
   const char *fragment_prog_file = "src/fragment.glsl";
-  _gl.shader_program =
-      CreateShaderProgram(vertex_prog_file, fragment_prog_file);
-
-  _window.width = width;
-  _window.height = height;
+  unsigned int shader_program = CreateShaderProgram(vertex_prog_file, fragment_prog_file);
 
   //initialize uniform variables
   float pos_mat[] = {
@@ -232,12 +209,49 @@ void CreateWindow(const int width, const int height, const char *title) {
 	0, 0, 0, 1.0f/_color_bit
   };
 
-  int proj_mat_pos = glGetUniformLocation(_gl.shader_program, "proj_pos_matrix");
-  int proj_mat_clr = glGetUniformLocation(_gl.shader_program, "proj_clr_matrix");
-  glUseProgram(_gl.shader_program);
+  int proj_mat_pos = glGetUniformLocation(shader_program, "proj_pos_matrix");
+  int proj_mat_clr = glGetUniformLocation(shader_program, "proj_clr_matrix");
+  glUseProgram(shader_program);
   glUniformMatrix4fv(proj_mat_pos, 1, GL_TRUE, pos_mat);
   glUniformMatrix4fv(proj_mat_clr, 1, GL_TRUE, clr_mat);
   glUseProgram(0);
+
+  // initialize memory for all gl_modes seperately
+  for (size_t i=0; i<MODE_SENTINEL; i++) {
+
+	  struct _gl_mode* ptr = &_gl_mode[i];
+	  glGenVertexArrays(1, &ptr->vao);
+	  glGenBuffers(1, &ptr->vbo);
+	  glGenBuffers(1, &ptr->ebo);
+
+	  glBindVertexArray(ptr->vao);
+	  glBindBuffer(GL_ARRAY_BUFFER, ptr->vbo);
+	  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ptr->ebo);
+
+	  // Note: Beware of these hardcoded numbers
+	  //
+	  // position
+	  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+	  glEnableVertexAttribArray(0);
+
+	  // color
+	  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+							(void *)(3 * sizeof(float)));
+	  glEnableVertexAttribArray(1);
+
+	  // texture
+	  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+							(void *)(7 * sizeof(float)));
+	  glEnableVertexAttribArray(2);
+
+	  ptr->sp = shader_program;
+	  glBindBuffer(GL_ARRAY_BUFFER, 0);
+	  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	  glBindVertexArray(0);
+  }
+
+  _window.width = width;
+  _window.height = height;
 }
 
 unsigned int CreateShaderProgram(const char *const vertex_glsl,
@@ -294,14 +308,9 @@ inline void EndCooking() {
   // clear screen
   // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glUseProgram(_gl.shader_program);
-  glBindVertexArray(_gl.vao);
-  glBindBuffer(GL_ARRAY_BUFFER, _gl.vbo);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _gl.ebo);
 
   // copy all gl modes data
   struct _gl_mode *ptr = nullptr;
-  size_t tot_vcap = 0, tot_icap = 0;
 
   for (size_t i = 0; i < MODE_SENTINEL; i++) {
     ptr = &_gl_mode[i];
@@ -323,21 +332,15 @@ inline void EndCooking() {
       assert(false, "Unknown draw call\n");
     }
 
-    glBufferSubData(GL_ARRAY_BUFFER, 0, ptr->vert_cnt * sizeof(*ptr->vertices),
-                    ptr->vertices);
-
-    glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0,
-                    ptr->idx_cnt * sizeof(*ptr->indices), ptr->indices);
+	glUseProgram(ptr->sp);
+	glBindVertexArray(ptr->vao);
+	glBindBuffer(GL_ARRAY_BUFFER, ptr->vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ptr->ebo);
 
     glDrawElements(draw_mode, ptr->idx_cnt, GL_UNSIGNED_INT, nullptr);
 
-    tot_vcap += ptr->vert_cnt;
-    tot_icap += ptr->idx_cnt;
-    ptr->idx_cnt = 0;
-    ptr->vert_cnt = 0;
+	ptr->vert_cnt = ptr->idx_cnt=0;
   }
-  assert(tot_vcap <= MAX_VERTICES, "Vertex Limit Exceeded\n");
-  assert(tot_icap <= MAX_INDICES, "Index Limit Exceeded\n");
   glfwSwapBuffers(_window.handle);
   glfwPollEvents();
   update_fps();
@@ -348,13 +351,9 @@ inline bool StartCooking() { return !glfwWindowShouldClose(_window.handle); }
 void CloseWindow() {
   glfwDestroyWindow(_window.handle);
   for (size_t i = 0; i < MODE_SENTINEL; i++) {
-    if (_gl_mode[i].vert_capacity)
-      free(_gl_mode[i].vertices);
-    if (_gl_mode[i].idx_capacity)
-      free(_gl_mode[i].indices);
+	  glDeleteVertexArrays(1, &_gl_mode[i].vao);
+	  glDeleteBuffers(1, &_gl_mode[i].vbo);
+	  glDeleteProgram(_gl_mode[i].sp);
   }
-  glDeleteVertexArrays(1, &_gl.vao);
-  glDeleteBuffers(1, &_gl.vbo);
-  glDeleteProgram(_gl.shader_program);
   glfwTerminate();
 }
