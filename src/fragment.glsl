e45ea5f1b7421a822260d1754191f1c3a7a7255b
Amrutanshu <amrubht11@gmail.com>
2024-12-29 23:25:56 +0530

    version change

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -3,9 +3,8 @@
 - 4th component of vec4 is opacity
 */
 
-#version 330 core
+#version 400 core
 
-uniform vec4 fragcolor;
 in vec4 cust_color;
 out vec4 colorout;
 
--- src/vertex.glsl
+++ src/vertex.glsl
@@ -1,7 +1,7 @@
 /*
 	proj_***_matrix: matrix transformation from local view to gl view
 */
-#version 330 core
+#version 400 core
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec4 vcolor;
 
